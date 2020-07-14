8a48bd2e149526570f6d6fbe56e0d3d6f32162c6
wangtie <wangtie@dushu.io>
2020-07-14 14:21:30 +0800

    Initial Commit

---
+++ GoodsPriceUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodsPriceUI
+//
+//  Created by wangtie on 2020/7/14.
+//  Copyright © 2020 wangtie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
